8177f563673a556defecec3b1630cb450ac34b05
murtaza-ghufoor <murtazaabdulghufoor@gmail.com>
2024-04-30 08:52:21 -0700

    first commit

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,44 @@
+using System;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public int Salary { get; set; }
+
+    public Employee(string name, int salary)
+    {
+        Name = name;
+        Salary = salary;
+    }
+}
+
+public class Manager : Employee
+{
+    public int Id { get; set; }
+    public string Designation { get; set; }
+
+    public Manager(int id, string name, int salary, string designation)
+        : base(name, salary)
+    {
+        Id = id;
+        Designation = designation;
+    }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Salary: {Salary}");
+        Console.WriteLine($"ID: {Id}");
+        Console.WriteLine($"Designation: {Designation}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Manager manager = new Manager(200414543, "AURANG ZAIB", 90000, "Manager");
+
+        manager.PrintDetails();
+    }
+}
